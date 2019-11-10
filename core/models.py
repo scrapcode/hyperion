12fec77d8ef065adbf32d4a804cd1e1fb2b12b7b
Cam Cecil <ctcecil@gmail.com>
2019-11-09 18:37:46 -0600

    House keeping

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .helpers import *
+from .helpers import two_weeks_from_today
 
-
+"""
+Project
+"""
 class Project(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
@@ -17,7 +19,9 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
-
+"""
+Task
+"""
 class Task(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=False)
@@ -26,8 +30,14 @@ class Task(models.Model):
 
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
 
+    def __str__(self):
+        return self.name + ' (' + project.name + ')'
 
+"""
+Followup
+"""
 class Followup(models.Model):
     description = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followups')
     send_date = models.DateField(default=two_weeks_from_today(), blank=False, null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
