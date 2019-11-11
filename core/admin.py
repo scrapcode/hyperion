0d58d10570707763bd93adc1a7a9b53ab34f635d
Cam Cecil <ctcecil@gmail.com>
2019-11-11 12:48:55 -0600

    Cleaning up the admin section.

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,20 @@ from django.contrib.auth import get_user_model
 from .models import Project
 
 
+admin.site.site_header = "Hyperion Admin"
+admin.site.site_title = "Hyperion Admin"
+admin.site.index_title = "Welcome to the Hyperion Administration section."
+
 class ProjectAdmin(admin.ModelAdmin):
     exclude = ('created_by',)
+    list_display = ('code', 'name', 'status', 'point_of_contact', 'created', 'updated')
+    list_filter = ('status', 'created', 'point_of_contact')
+    fields = (
+        ('name', 'code', 'status'),
+        'description',
+        'point_of_contact',
+    )
+    search_fields = ['name', 'code']
 
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user
--- core/models.py
+++ core/models.py
@@ -6,8 +6,28 @@ from .helpers import two_weeks_from_today
 Project
 """
 class Project(models.Model):
+    ACTIVE = 'AC'
+    COMPLETE = 'CL'
+    CANCELLED = 'CA'
+
+    LIST_OF_CHOICES = [
+        (ACTIVE, 'Active'),
+        (COMPLETE, 'Completed'),
+        (CANCELLED, 'Cancelled')
+    ]
+
     name = models.CharField(max_length=256)
+    code = models.CharField(
+        max_length=16,
+        blank=True,
+        null=True
+    )
     description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=2,
+        choices=LIST_OF_CHOICES,
+        default=ACTIVE
+    )
 
     point_of_contact = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
 
